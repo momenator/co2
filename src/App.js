f1efc1d9205fd1731bc147cd89cfa45171cefd04
tamaramueller <1204tamara@online.de>
2019-12-07 12:11:36 +0100

    exploring java script

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import logo from './logo.svg'
 
 function App() {
+
+  const [ colour, changeColour ] = useState("blue")
   return (
-    <div className="App">
-      <header className="App-header">
+
+    <html>
+      <body>
+
+      <div className="App">
         <p>
-          App here!
+          <h1>Our Cool Name</h1>
         </p>
-      </header>
+
+        <p id="demo" style={{ color: colour }}>dafdsaf</p>
+        <button type="button" onClick={() => { changeColour("red") }}>Click Me!</button>
+        </div>
+
+      <div>
+        <p>
+          <IdiomaticReactList items={[10000,24456,3]}/>
+        </p>
+      </div>
+
+      </body>
+
+      <div>
+          <img src={logo} />
+      </div>
+
+    </html>
+  );
+}
+
+function IdiomaticReactList(props) {
+  return (
+    <div>
+      {props.items.map((item, index) => (
+        <div align="center" key={index} >
+          {index} | {item}
+        </div>
+      ))}
     </div>
   );
 }
