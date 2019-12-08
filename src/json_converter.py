5b6e40bba16796e9497065f20f596af40eae24fa
tamaramueller <1204tamara@online.de>
2019-12-08 07:22:41 +0100

    added csv to json converter python

---
+++ src/json_converter.py
@@ -0,0 +1,28 @@
+import sys, getopt
+import csv
+import json
+
+#Get Command Line Arguments
+def convert_cvs_to_json(input_file, output_file):
+    read_csv(input_file, output_file, format)
+
+#Read CSV File
+def read_csv(file, json_file, format):
+    csv_rows = []
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        title = reader.fieldnames
+        for row in reader:
+            csv_rows.extend([{title[i]:row[title[i]] for i in range(len(title))}])
+        write_json(csv_rows, json_file, format)
+
+#Convert csv data into json and write it
+def write_json(data, json_file, format):
+    with open(json_file, "w") as f:
+        if format == "pretty":
+            f.write(json.dumps(data, sort_keys=False, indent=4, separators=(',', ': '),encoding="utf-8",ensure_ascii=False))
+        else:
+            f.write(json.dumps(data))
+
+if __name__ == "__main__":
+   convert_cvs_to_json(input_file="./data/conversion_factors.csv", output_file="./data/conversion_factors.json")
